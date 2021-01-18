2dddcee8050c31029b909b7c84c3fbce5bc87221
Alexandru Ionescu <33354331+al3xionescu@users.noreply.github.com>
2021-01-18 18:52:47 +0200

    Update 04.sql

--- 04.sql
+++ 04.sql
@@ -1,1 +1,2 @@
-SELECT exams.student from exams GROUP BY exams.student HAVING CAST(COUNT(exams.student) * 3 as float) / 10 < (SELECT COUNT(student) from exams E where E.student = exams.student and E.grade < 40 GROUP BY E.student);
+SELECT exams.student from exams GROUP BY exams.student HAVING CAST(COUNT(exams.student) * 3 as float) / 10 < (SELECT COUNT(student) from exams E 
+where E.student = exams.student and E.grade < 40 GROUP BY E.student);
