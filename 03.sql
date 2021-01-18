fbdbb0d0c03b24f3201d14f7f5e9f7bb5e90f39e
Alexandru Ionescu <33354331+al3xionescu@users.noreply.github.com>
2021-01-18 18:53:09 +0200

    Update 03.sql

--- 03.sql
+++ 03.sql
@@ -1,1 +1,2 @@
-SELECT exams.student, MIN(exams.grade), MAX(exams.grade), COUNT(exams.student) from exams JOIN students ON students.uun = exams.student GROUP BY exams.student HAVING CAST (SUM(exams.grade) AS FLOAT) / COUNT(exams.grade) >= 75;
+SELECT exams.student, MIN(exams.grade), MAX(exams.grade), COUNT(exams.student) from exams JOIN students ON students.uun = exams.student 
+GROUP BY exams.student HAVING CAST (SUM(exams.grade) AS FLOAT) / COUNT(exams.grade) >= 75;
